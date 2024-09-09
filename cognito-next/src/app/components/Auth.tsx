0556c73557ea5167614d9f3f358d7474577237ed
kmjak <tymkoron1221@icloud.com>
2024-09-09 23:51:46 +0900

    AWS側の設定がわからん

---
+++ cognito-next/src/app/components/Auth.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import { useState } from "react"
+import { LoginForm } from "./LoginForm"
+import { SignupForm } from "./SignupForm"
+
+export const AuthComponent = () => {
+  const [authMode, setAuthMode] = useState<"login" | "signup">("login")
+  return(
+    <>
+      {authMode === "login" ? (
+        <>
+          <LoginForm />
+          <button
+          className="text-blue-500"
+          onClick={() => setAuthMode("signup")}
+        >
+          サインアップ
+        </button>
+      </>
+      ) : (
+        <>
+        <SignupForm />
+          <button
+          className="text-blue-500"
+          onClick={() => setAuthMode("login")}
+        >
+          ログイン
+        </button>
+      </>
+      )}
+      <div className="flex justify-center mt-4 h-1/6">
+
+      </div>
+    </>
+  )
+}
---
+++ cognito-next/src/app/components/LoginForm.tsx
@@ -0,0 +1,35 @@
+export const LoginForm = () => {
+  return (
+    <div className="flex flex-col justify-center min-h-5/6">
+      <h2 className="text-4xl font-bold text-center">ログイン</h2>
+      <form className="mt-4">
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            ユーザーネーム
+          </label>
+          <input
+            type="text"
+            name="text"
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-700">
+            パスワード
+          </label>
+          <input
+            type="password"
+            name="password"
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          ログイン
+        </button>
+      </form>
+    </div>
+  );
+}
